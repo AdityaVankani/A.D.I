ea6655609b5ad37f1ffcb30c7acba8fc72e869a7
AdityaVankani <adivankani@gmail.com>
2025-06-23 16:04:12 +0530

    A.D.I update3

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+CORS(app, supports_credentials=True, origins=["http://localhost:5173","https://adi-terminal.vercel.app"])
 
 @app.route("/")
 def home():
